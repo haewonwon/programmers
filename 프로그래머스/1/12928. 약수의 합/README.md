bd9a2b5ae0fdf0333a3d0b893a9f87217fcd9a3c
해원 <khcw1029@daum.net>
2025-04-23 00:02:48 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/README.md
+++ 프로그래머스/1/12928. 약수의 합/README.md
@@ -1,6 +1,6 @@
 # [level 1] 약수의 합 - 12928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 21일 23:36:56
+2025년 04월 23일 00:02:45
 
 ### 문제 설명
 
--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,22 @@
 function solution(n) {
-    let sumVal = 0;
-    
-    for (let i = 0; i <= n; i++) {
-        if (n % i === 0) {
-            sumVal += i
-        }
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) sum += i;
     }
-    
-    return sumVal;
+
+    return sum;
 }
+
+
+// function solution(n) {
+//     let sumVal = 0;
+    
+//     for (let i = 0; i <= n; i++) {
+//         if (n % i === 0) {
+//             sumVal += i
+//         }
+//     }
+    
+//     return sumVal;
+// }
