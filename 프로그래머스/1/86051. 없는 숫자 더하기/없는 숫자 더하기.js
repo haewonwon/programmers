c8d3f62e9a96bb8a9a8a80c17b7ab9d126eabfe2
해원 <khcw1029@daum.net>
2025-04-26 23:35:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    return arr.filter(x => !numbers.includes(x)).reduce((sum, digit) => sum + digit, 0);
+    
+    
+}
